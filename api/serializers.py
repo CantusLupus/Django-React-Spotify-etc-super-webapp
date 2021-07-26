f046b481d0152dfe7c02faf15aeb0de7243a0ed6
CantusLupus <i.s.work234@gmail.com>
2021-07-26 12:03:46 +0200

    git init

---
+++ api/serializers.py
@@ -0,0 +1,8 @@
+from django.db.models import fields
+from rest_framework import serializers
+from .models import Room
+
+class RoomSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Room
+    fields = ('id', 'code', 'host', 'guest_can_pause', 'guest_can_skip', 'created_at')
