b59b6ad2293df6dda2aa2885a7b4dc531d79be9f
CantusLupus <i.s.work234@gmail.com>
2021-07-28 22:44:35 +0200

    create room backend

--- api/models.py
+++ api/models.py
@@ -7,7 +7,7 @@ def generate_code():
 
   while True:
     code = ''.join(random.choices(string.ascii_uppercase, k=6))
-    if Room.objests.filter(code=code).count() == 0:
+    if Room.objects.filter(code=code).count() == 0:
       break
 
   return code
@@ -15,11 +15,8 @@ def generate_code():
 
 # Create your models here.
 class Room(models.Model):
-  code = models.CharField(max_length=6, default='', unique=True)
+  code = models.CharField(max_length=6, default=generate_code, unique=True)
   host = models.CharField(unique=True, max_length=50)
   guest_can_pause = models.BooleanField(null=False, default=False)
   guest_can_skip = models.BooleanField(null=False, default=False)
   created_at = models.DateTimeField(auto_now_add=True)
-
-  def is_host_this():
-    pass
--- api/serializers.py
+++ api/serializers.py
@@ -6,3 +6,8 @@ class RoomSerializer(serializers.ModelSerializer):
   class Meta:
     model = Room
     fields = ('id', 'code', 'host', 'guest_can_pause', 'guest_can_skip', 'created_at')
+
+class CreateRoomSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Room
+    fields = ('guest_can_pause', 'guest_can_skip')
