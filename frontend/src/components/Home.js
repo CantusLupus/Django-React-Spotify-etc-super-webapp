d06b7065405e6c2ffbf8961fef55c637b13a3bdb
CantusLupus <i.s.work234@gmail.com>
2021-08-29 18:20:46 +0300

    staying and leaving the room

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,20 +3,76 @@ import RoomJoinPage from "./RoomJoin";
 import CreateRoomPage from "./CreateRoom"; 
 import Room from "./Room";
 import { BrowserRouter as Router, Switch, Route, Link, Redirect} from "react-router-dom";
+import { ButtonGroup, Button, Grid, Typography } from "@material-ui/core";
 
 export default class HomePage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      roomCode: null,
+    };
+    this.clearRoomCode = this.clearRoomCode.bind(this);
+  }
+
+  async componentDidMount() {
+    fetch('/api/user-in-room')
+    .then((response) => response.json())
+    .then((data) => {
+      this.setState({
+        roomCode: data.code
+      });
+    });
+  }
+
+  renderHomePage() {
+    return (
+      <Grid container spacing={3}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h3" component="h3">
+            Co-music
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <ButtonGroup disableElevation variant="contained" color="primary">
+              <Button variant="contained" color="secondary" to="/join" component={Link}>
+                Join
+                </Button>
+              <Button variant="contained" color="primary" to="/create" component={Link}>
+                Create
+                </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    );
+  }
+
+  clearRoomCode() {
+    this.setState({
+      roomCode: null,
+    });
   }
 
   render() {
-    return <Router>
-      <Switch>
-        <Route exact path="/">Home Page</Route>
-        <Route exact path="/join" component={RoomJoinPage} />
-        <Route exact path="/create" component={CreateRoomPage} />
-        <Route exact path="/room/:roomCode" component={Room} />
-      </Switch>
-    </Router>;
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" render={() => {
+            return this.state.roomCode ? (
+              <Redirect to={`/room/${this.state.roomCode}`} />
+            ) : (
+              this.renderHomePage()
+            );
+          }} 
+          />
+          <Route exact path="/join" component={RoomJoinPage} />
+          <Route exact path="/create" component={CreateRoomPage} />
+          <Route exact path="/room/:roomCode" 
+          render={(props) => {
+            return <Room {...props} leaveRoomCallback={this.clearRoomCode} />;
+          }}
+          />
+        </Switch>
+    </Router>
+    );
   }
 }
--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { TextField, Button, Grid, Typography } from "@material-ui/core";
 
 export default class room extends Component {
   constructor(props) {
@@ -10,12 +11,19 @@ export default class room extends Component {
     };
     this.roomCode = this.props.match.params.roomCode;
     this.getRoomDetails();
+    this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
   }
 
   getRoomDetails() {
-    fetch('/api/get-room' + '?code=' + this.roomCode).then((response) =>
-     response.json()
-     ).then((data) => {
+    return fetch('/api/get-room' + '?code=' + this.roomCode)
+      .then((response) => {
+        if (!response.ok) {
+          this.props.leaveRoomCallback();
+          this.props.history.push('/');
+        }
+        return response.json();
+      })
+      .then((data) => {
        this.setState({
          guestCanPause: data.guest_can_pause,
          guestCanSkip: data.guest_can_skip,
@@ -24,13 +32,46 @@ export default class room extends Component {
      });
   }
 
+  leaveButtonPressed() {
+    const requestOptions = {
+      method: 'POST',
+      headers: {"Content-Type": "application/json"},
+    };
+    fetch('/api/leave-room', requestOptions).then((_response) => {
+      this.props.leaveRoomCallback();
+      this.props.history.push('/');
+    });
+  }
+
   render() {
-    return <div>
-      <h3>{this.roomCode}</h3>
-      <p>Can skip: {this.state.guestCanSkip.toString()}</p>
-      <p>Can pause: {this.state.guestCanPause.toString()}</p>
-      <p>Host: {this.state.isHost.toString()}</p>
-    </div>
+    return (
+    <Grid container spacing={1}>
+      <Grid item xs={12} align="center">
+        <Typography variant="h6" component="h6">
+          Code: {this.roomCode}
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+      <Typography variant="h6" component="h6">
+          Guest can pause: {this.state.guestCanPause.toString()}
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+      <Typography variant="h6" component="h6">
+          Guest can skip: {this.state.guestCanSkip.toString()}
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+      <Typography variant="h6" component="h6">
+      Host: {this.state.isHost.toString()}
+      </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+      <Button variant="contained" color="secondary" to="/" onClick={this.leaveButtonPressed}>
+        Leave Room
+        </Button>
+      </Grid>
+    </Grid>
+    )
   }
 } 
-
